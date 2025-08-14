cd7b40cf17e7858215e5ede8546316ef9cb4ecfc
ry0218 <ry0218@naver.com>
2025-08-14 14:41:28 +0900

    chore: 아이콘 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,14 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toast";
 
+export const metadata = {
+  title: "찰푸 리뷰",
+  description: "솔직한 피드백으로 맛집을 더 맛있게",
+  icons: {
+    icon: "/icon.svg",
+  },
+};
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
